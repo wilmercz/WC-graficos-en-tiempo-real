0f812862494673ccf3fc1bf070e366c665662030
wilmercz <49212228+wilmercz@users.noreply.github.com>
2024-09-22 10:21:58 -0500

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// firebase-config.js
+   export const firebaseConfig = {
+     apiKey: "AIzaSyD0lkEDKhia76E6ef3uU0B1lOH8CyklCnc",
+     authDomain: "arki-service-autoridades.firebaseapp.com",
+     databaseURL: "https://arki-service-autoridades.firebaseio.com",
+     projectId: "arki-service-autoridades",
+     storageBucket: "arki-service-autoridades.appspot.com",
+     messagingSenderId: "829954736380",
+     appId: "1:829954736380:web:c4badfee0e0db02dd69779"
+   };
