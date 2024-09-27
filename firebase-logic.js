0d794b8e5ac711445339c50ec6494b0be8c86d12
wilmercz <49212228+wilmercz@users.noreply.github.com>
2024-09-27 17:23:46 -0500

    Update firebase-logic.js

--- firebase-logic.js
+++ firebase-logic.js
@@ -112,7 +112,7 @@ document.getElementById('status').innerText = 'PASO 4';
       //} else {
       //  logo.style.display = 'none';
       //}
-      
+      document.getElementById('status').innerText = logoUrl;
       if (logoAlAire && logoUrl) {
         logo.src = logoUrl;
         logo.style.display = 'block';
